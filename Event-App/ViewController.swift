704c636f260b963cd58c831c74a1841124e7169d
Burak AKCAN <106486885+BurakAkcan@users.noreply.github.com>
2022-10-18 11:38:10 +0300

    Initial Commit

---
+++ Event-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Event-App
+//
+//  Created by Burak AKCAN on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
